1bdcd1f3a6881afc79417fad624fba66ac9534db
emilvn <emilvnielsen@hotmail.com>
2023-10-23 19:30:52 +0200

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,10 @@ import {SignIn, SignInButton, SignOutButton, useUser} from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+
+  const {data} = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -20,7 +21,11 @@ export default function Home() {
             {!user.isSignedIn && <SignInButton />}
             {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up"/>
+        <div>
+            {data?.map((post) => (
+                <div key={post.id}>{post.content}</div>
+            ))}
+        </div>
       </main>
     </>
   );
